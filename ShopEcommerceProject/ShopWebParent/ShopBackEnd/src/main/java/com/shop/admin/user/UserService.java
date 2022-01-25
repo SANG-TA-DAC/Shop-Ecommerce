22876148c169c8ea6db08e2bf2369e088224b617
tadacsang <dongianthoi83@gmail.com>
2022-01-25 15:52:12 +0700

    Encode User Password and Unit Tests

--- ShopEcommerceProject/ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/user/UserService.java
+++ ShopEcommerceProject/ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/user/UserService.java
@@ -3,6 +3,7 @@ package com.shop.admin.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.shop.common.entity.Role;
@@ -16,6 +17,9 @@ public class UserService {
 	@Autowired
 	private RoleRepository roleRepo;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	// show list all Users
 	public List<User> listAll(){
 		return (List<User>) userRepo.findAll();
@@ -28,6 +32,13 @@ public class UserService {
 	
 	// create a new User
 	public void save(User user) {
+		encodePassword(user);
 		userRepo.save(user);
+	}
+	
+	// encode password
+	private void encodePassword(User user) {
+		String encodePassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodePassword);
 	}
 }
