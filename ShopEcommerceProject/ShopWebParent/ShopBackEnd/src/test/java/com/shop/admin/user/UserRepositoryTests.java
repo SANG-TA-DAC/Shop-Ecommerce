eb4259fdfb7eb71022a7d448c862812169ca0169
tadacsang <dongianthoi83@gmail.com>
2022-01-27 11:13:31 +0700

    Code Pagination For User List

--- ShopEcommerceProject/ShopWebParent/ShopBackEnd/src/test/java/com/shop/admin/user/UserRepositoryTests.java
+++ ShopEcommerceProject/ShopWebParent/ShopBackEnd/src/test/java/com/shop/admin/user/UserRepositoryTests.java
@@ -2,6 +2,7 @@ package com.shop.admin.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -10,12 +11,15 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.Rollback;
 
 import com.shop.common.entity.Role;
 import com.shop.common.entity.User;
 
-@DataJpaTest
+@DataJpaTest(showSql = false)
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @Rollback(false) 
 public class UserRepositoryTests {
@@ -116,5 +120,20 @@ public class UserRepositoryTests {
 	public void testEnableUser() {
 		Integer id = 1;
 		repo.updateEnabledStatus(id, true);
+	}
+	
+	@Test
+	public void testListFirstPage() {
+		int pageNumber = 0;
+		int pageSize = 4;
+		
+		Pageable pageable = PageRequest.of(pageNumber, pageSize);
+		Page<User> page = repo.findAll(pageable);
+		
+		List<User> listUsers = page.getContent();
+		
+		listUsers.forEach(user -> System.out.println(user));
+		
+		assertThat(listUsers.size()).isEqualTo(pageSize);
 	}
 }
