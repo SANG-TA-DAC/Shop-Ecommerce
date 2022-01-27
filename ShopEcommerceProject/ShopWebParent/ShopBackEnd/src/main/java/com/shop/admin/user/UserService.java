440c9b251d8f8eee8d00385a0732a7fd4aed7fc7
tadacsang <dongianthoi83@gmail.com>
2022-01-27 15:51:58 +0700

    Code Sorting For Users List

--- ShopEcommerceProject/ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/user/UserService.java
+++ ShopEcommerceProject/ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/user/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,89 +19,95 @@ import com.shop.common.entity.User;
 @Service
 @Transactional
 public class UserService {
-	
+
 	public static final int USERS_PER_PAGE = 3;
-	
+
 	@Autowired
 	private UserRepository userRepo;
-	
+
 	@Autowired
 	private RoleRepository roleRepo;
-	
+
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
+
 	// show list all Users
-	public List<User> listAll(){
+	public List<User> listAll() {
 		return (List<User>) userRepo.findAll();
 	}
-	
-	public Page<User> listByPage(int pageNum){
-		Pageable pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE);
+
+	public Page<User> listByPage(int pageNum, String sortField, String sortDir) {
+		Sort sort = Sort.by(sortField);
+		
+		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+		
+		Pageable pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE, sort);
+		
 		return userRepo.findAll(pageable);
 	}
-	
+
 	// show list all Roles
-	public List<Role> listRoles(){
+	public List<Role> listRoles() {
 		return (List<Role>) roleRepo.findAll();
 	}
-	
-	
+
 	public User save(User user) {
 		boolean isUpdatingUser = (user.getId() != null);
-		
+
 		if (isUpdatingUser) {
 			User existingUser = userRepo.findById(user.getId()).get();
 			if (user.getPassword().isEmpty()) {
 				user.setPassword(existingUser.getPassword());
-			}else {
+			} else {
 				encodePassword(user);
 			}
-		}else {
+		} else {
 			encodePassword(user);
 		}
 		return userRepo.save(user);
 	}
-	
+
 	// encode password
 	private void encodePassword(User user) {
 		String encodePassword = passwordEncoder.encode(user.getPassword());
 		user.setPassword(encodePassword);
 	}
-	
+
 	public boolean isEmailUnique(Integer id, String email) {
 		User userByEmail = userRepo.getUserByEmail(email);
-		
-		if(userByEmail == null) return true;
-		
+
+		if (userByEmail == null)
+			return true;
+
 		boolean isCreatingNew = (id == null);
-		
+
 		if (isCreatingNew) {
-			if(userByEmail != null) return false;
-		}else {
-			if(userByEmail.getId() != id) {
+			if (userByEmail != null)
+				return false;
+		} else {
+			if (userByEmail.getId() != id) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
-	public User get(Integer id) throws UserNotFoundException{
+
+	public User get(Integer id) throws UserNotFoundException {
 		try {
 			return userRepo.findById(id).get();
-		}catch (NoSuchElementException ex) {
+		} catch (NoSuchElementException ex) {
 			throw new UserNotFoundException("Could not find any user with ID " + id);
 		}
 	}
-	
+
 	public void delete(Integer id) throws UserNotFoundException {
 		Long countById = userRepo.countById(id);
-		if(countById == null || countById == 0) {
+		if (countById == null || countById == 0) {
 			throw new UserNotFoundException("Could not find any user with ID " + id);
 		}
 		userRepo.deleteById(id);
 	}
-	
+
 	public void updateUserEnabledStatus(Integer id, boolean enabled) {
 		userRepo.updateEnabledStatus(id, enabled);
 	}
