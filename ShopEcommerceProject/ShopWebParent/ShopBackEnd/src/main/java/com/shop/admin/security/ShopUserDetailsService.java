092d8423061e35b72dbf545554570d2d02bc768a
tadacsang <dongianthoi83@gmail.com>
2022-02-24 11:22:24 +0700

    Code User Authentication

---
+++ ShopEcommerceProject/ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/security/ShopUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.shop.admin.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.shop.admin.user.UserRepository;
+import com.shop.common.entity.User;
+
+public class ShopUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepo.getUserByEmail(email);
+		
+		if (user != null) {
+			return new ShopUserDetails(user);
+		}
+		
+		throw new UsernameNotFoundException("Could not find user with email: " + email);
+		
+	}
+
+}
