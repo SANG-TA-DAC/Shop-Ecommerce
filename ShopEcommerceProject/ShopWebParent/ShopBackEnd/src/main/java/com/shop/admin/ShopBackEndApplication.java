7279186eec75e047a9b621f25011c576025b0e69
tadacsang <dongianthoi83@gmail.com>
2022-03-01 15:10:39 +0700

    Configure Logging For ShopAdmin

--- ShopEcommerceProject/ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/ShopBackEndApplication.java
+++ ShopEcommerceProject/ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/ShopBackEndApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan({"com.shop.common.entity", "com.shop.admin.user"})
+@EntityScan({"com.shop.common.entity"})
 public class ShopBackEndApplication {
 
 	public static void main(String[] args) {
